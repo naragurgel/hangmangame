00d089f4a8fc260eeeeefdf930cefe27ff4efb6c
naragurgel <gurgel.nara@gmail.com>
2023-01-04 20:01:09 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,3 +1,24 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+from words import word_list
+
+def choose_word():
+    """
+    Gets a random word from the list 'words' and converts the user input to uppercase to make easer the comparison logic
+    """
+    word = random.choice(word_list)
+    return word.upper()
+
+def start_game(word):
+    """
+    represent unguesses letters as underscores and show letter as correct guesses
+    """
+    word_completion = "_" * len(word)
+    certain = False
+    certain_letters = []
+    certain_words = []
+    attempts = 6
+    print("Are you ready to play Hangman? Let's get started!")
+    print(display_hangman(attempts))
+    print(word_completion)
+    print("\n")
+
