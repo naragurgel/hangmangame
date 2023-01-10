1977f458b173a53e54474e911ff3f104be4f0210
naragurgel <gurgel.nara@gmail.com>
2023-01-10 22:58:53 +0000

    add function to explain better the strutute of the code

--- run.py
+++ run.py
@@ -1,11 +1,13 @@
 import random
+import string
 from words import word_list
+from stages import STAGES
 
 class HangmanGame:
     """
     Data Model for Hangman Game
 
-    ***
+    *
     Attributes
     ----------
     word: str
@@ -14,169 +16,106 @@ class HangmanGame:
         display of word with blanks for user's guesses
     certain: Boolean
         tracks if user has guessed current word or not
-    certain_letters: array of str
+    guessed_letters: array of str
         tracks letters user has guessed 
-    certain_words: array of strings
+    guessed_words: array of strings
         tracks words user has guessed
     attempts: int
         count down of attempts untl user looses game
     """
-    
+
     def __init__(self):
         self.word = ""
-        self.word_completion = ""
-        self.certain = False
-        self.certain_letters = []
-        self.certain_words = []
+        self.word_displayed = ""
+        self.game_completed = False
+        self.guessed_letters = []
+        self.guessed_words = []
         self.attempts = 6
+        self.available_letters = set(string.ascii_uppercase)
 
+    def choose_word(self):
+        """
+        Gets a random word from the list 'words' and converts the user input to uppercase to make easer the comparison logic
+        """
+        self.word = random.choice(word_list).upper()
+        self.word_displayed = "_" * len(self.word)
 
-def choose_word():
-    """
-    Gets a random word from the list 'words' and converts the user input to uppercase to make easer the comparison logic
-    """
-    word = random.choice(word_list)
-    return word.upper()
+    def start_game(self):
+        """
+        represent unguesses letters as underscores and show letter as correct guesses #noqa
+        """
+        print("Are you ready to play Hangman? Let's get started!")
+        self.display_hangman()
+        """
+        the loops runs till the word is guessed or the users runs out of guessed_letter
+        """
+        while not self.game_completed and self.attempts > 0:
+            guess = input("Please guess a letter or word: \n").upper()
+            if len(guess) == 1 and guess.isalpha():
+                self.resolve_guessed_letter(guess)
 
-def start_game(word):
-    """
-    represent unguesses letters as underscores and show letter as correct guesses
-    """
-    word_completion = "_" * len(word)
-    certain = False
-    certain_letters = []
-    certain_words = []
-    attempts = 6
-    print("Are you ready to play Hangman? Let's get started!")
-    print(display_hangman(attempts))
-    print(word_completion)
-    print("\n")
-    """
-    the loops runs till the word is guessed or the users runs out of tries
-    """
-    while not certain and attempts > 0:
-        tries = input("Please guess a letter or word: \n").upper()
-        if len(tries) == 1 and tries.isalpha():
-            if tries in certain_letters:
-                print("You alredy guessed the letter", tries)
-            elif tries not in word:
-                print(tries, "is not in the word.")
-                attempts -= 1
-                certain_letters.append(tries)
+            elif len(guess) == len(self.word) and guess.isalpha():
+                self.resolve_guessed_word(guess)
             else:
-                print("Well done,", tries, "is in the word!")
-                certain_letters.append(tries)
-                word_as_list = list(word_completion)
-                indices = [i for i, letter in enumerate(word) if letter == tries]
-                for index in indices:
-                    word_as_list[index] = tries
-                word_completion = "".join(word_as_list)
-                if "_" not in word_completion:
-                    certain = True
-        elif len(tries) == len(word) and tries.isalpha():
-            if tries in certain_words:
-                print("You alredy guessed the word", guess)
-            elif tries != word:
-                print(tries, "is not the word.")
-                attempts -= 1
-                certain_words.append(tries)
-            else:
-                certain = True
-                word_completion = word
+                print("Not a valid guess.")
+            self.display_hangman()
 
-
+        if self.game_completed:
+            print("God job, you guessed the word! You won!")
         else:
-            print("Not a valid guess.")
-        print(display_hangman(attempts))
-        print(word_completion)
+            print("Unfortunately, you ran out of guesses. The word was " + self.word + ". Maybe next time!")  # noqa
+
+    def display_hangman(self):
+        print(f"Available Letters: {' '.join(sorted(self.available_letters))}")
+        print(STAGES[self.attempts])
+        print(self.word_displayed)
+        print(f"Tries left: {self.attempts}")
+        print(f'Letters guessed: {" ".join(self.guessed_letters)}')
+        print(f'Words guessed: {" ".join(self.guessed_words)}')
         print("\n")
 
-    if certain:
-        print("God job, you guessed the word! You win!")
+    def resolve_guessed_letter(self, guessed_letter):
+        if guessed_letter in self.guessed_letters:
+            print(f"You already guessed the letter {guessed_letter}")
+        elif guessed_letter not in self.word:
+            print(f"{guessed_letter} is not in the word.")
+            self.attempts -= 1
+        else:
+            print(f"Well done, {guessed_letter} is in the word!")
+            self.reveal_letters(guessed_letter)
+        self.guessed_letters.append(guessed_letter)
+        self.available_letters -= {guessed_letter}
 
-    else:
-        print("Unfortunately, you ran out of tries. The word was " + word + ". Maybe next time!")
+    def resolve_guessed_word(self, guessed_word):
+        if guessed_word in self.guessed_words:
+            print("You already guessed the word", guessed_word)
+        elif guessed_word != self.word:
+            print(guessed_word, "is not the word.")
+            self.attempts -= 1
+            self.guessed_words.append(guessed_word)
+        else:
+            self.game_completed = True
+            self.word_displayed = self.word
 
-def display_hangman(tries):
-    stages = [  # final state: head, torso, both arms, and both legs
-                """
-                   --------
-                   |      |
-                   |      O
-                   |     \\|/
-                   |      |
-                   |     / \\
-                   -
-                """,
-                # head, torso, both arms, and one leg
-                """
-                   --------
-                   |      |
-                   |      O
-                   |     \\|/
-                   |      |
-                   |     / 
-                   -
-                """,
-                # head, torso, and both arms
-                """
-                   --------
-                   |      |
-                   |      O
-                   |     \\|/
-                   |      |
-                   |      
-                   -
-                """,
-                # head, torso, and one arm
-                """
-                   --------
-                   |      |
-                   |      O
-                   |     \\|
-                   |      |
-                   |     
-                   -
-                """,
-                # head and torso
-                """
-                   --------
-                   |      |
-                   |      O
-                   |      |
-                   |      |
-                   |     
-                   -
-                """,
-                # head
-                """
-                   --------
-                   |      |
-                   |      O
-                   |    
-                   |      
-                   |     
-                   -
-                """,
-                # initial empty state
-                """
-                   --------
-                   |      |
-                   |      
-                   |    
-                   |      
-                   |     
-                   -
-                """
-    ]
-    return stages[tries]
+    def reveal_letters(self, guessed_letter):
+        word_as_list = list(self.word_displayed)
+        indices = [i for i, letter in enumerate(self.word) if letter == guessed_letter]
+        for index in indices:
+            word_as_list[index] = guessed_letter
+        self.word_displayed = "".join(word_as_list)
+        if "_" not in self.word_displayed:
+            self.game_completed = True
 
 def main():
-    word = choose_word()
-    start_game(word)
-    while input("Play Again? (Y/N) \n"). upper() == "Y":
-        word = choose_word()
-        start_game(word)
+    game = HangmanGame()
+    game.choose_word()
+    game.start_game()
+
+    while input("Play Again? (Y/N) \n").upper() == "Y":
+        game = HangmanGame()
+        game.choose_word()
+        game.start_game()
+
 
 if __name__ == "__main__":
     main()
